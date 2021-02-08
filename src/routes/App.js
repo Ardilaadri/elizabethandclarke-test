234a48a4955c6fa28e0b4fadb284de6fb8f931b1
Adriana Ardila <adriana2014lucia@outlook.com>
2021-02-08 16:33:37 -0500

    assets

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Searcher from "../components/Searcher";
-import Products from "../components/Products";
-import Results from "../components/Results";
 import "../assets/styles/App.scss";
 
 function App() {
@@ -11,8 +7,6 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Route path="/" component={Searcher} />
-          <Route path="/items?search=" component={Products} />
-          <Route path="/items/:id" component={Results} />
         </BrowserRouter>
       </div>
     </>
