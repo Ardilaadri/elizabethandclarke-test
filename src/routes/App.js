bff52c36c9624b66df2fbef7c0b42e0939cbe359
Adriana Ardila <adriana2014lucia@outlook.com>
2021-02-08 17:55:34 -0500

    show all components

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
+import Welcome from "../components/Welcome";
+import Main from "../components/Main";
+import Article from "../components/Article";
 import "../assets/styles/App.css";
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
       <body>
         <div className="App">
           <Header />
+          <Welcome />
+          <Main />
+          <Article />
         </div>
       </body>
     </>
