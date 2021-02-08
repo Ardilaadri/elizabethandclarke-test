70b9dd9535aabe9e4b78a7c682e483fa4395f9b8
Adriana Ardila <adriana2014lucia@outlook.com>
2021-02-08 17:11:13 -0500

    article section

---
+++ src/components/Article.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/styles/components/Article.css";
+
+const Article = () => {
+  <article>
+    <div className="items">
+      <div className="item"></div>
+      <div className="item"></div>
+    </div>
+  </article>;
+};
+
+export default Article;
