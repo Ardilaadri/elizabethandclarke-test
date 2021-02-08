d74b30034582e033b6ddb4865ec88d094494edc0
Adriana Ardila <adriana2014lucia@outlook.com>
2021-02-08 17:11:27 -0500

    main section

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/styles/components/Main.css";
+
+const Main = () => {
+  <section className="main">
+    <h1>It's What's Inside That Counts</h1>
+    <p>
+      We don't just make clothes, we solve problems. Every Piece
+      <br />
+      has smart details to make your life easier
+    </p>
+  </section>;
+};
+
+export default Main;
